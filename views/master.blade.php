49c0d6e4f816ab61c950386a2d8ea542bb21ad42
Galicz Miklós <gnick666@gmail.com>
2016-06-21 14:56:38 +0200

    View fix

--- views/master.blade.php
+++ views/master.blade.php
@@ -28,7 +28,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">{{Config::get('crud.title.long')}}</a>
+            <a class="navbar-brand" href="/">{{Config::get('crud.title')}}</a>
         </div>
         <div id="navbar-primary" class="collapse navbar-collapse">
             {{-- Menu generator comes here --}}
